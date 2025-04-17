cd31fb4af902d3e44e1ceb2909be77abac77b077
samirbenabdallah <samirbenabdallah4002@gmail.com>
2025-04-18 01:47:16 +0200

    update auth routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Loader from "./pages/Loader";
 import { isLoggedIn } from "./api/user";
 import { Route, Routes } from "react-router-dom";
 import Nav from "./pages/com/Nav";
+import Register from "./pages/Register";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -23,17 +24,19 @@ function App() {
   return (
     <div className="w-full h-screen ">
       <Nav />
-      <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/login" element={<h1>Login</h1>} />
-        <Route path="/register" element={<h1>Register</h1>} />
-        <Route path="/dashboard" element={<h1>Dashboard</h1>} />
-        <Route path="/profile" element={<h1>Profile</h1>} />
-        <Route path="/settings" element={<h1>Settings</h1>} />
-        <Route path="*" element={<h1>404 Not Found</h1>} />
-        <Route path="/:userId" element={<h1>User Profile</h1>} />
-        <Route path="/:userId/edit" element={<h1>Edit Profile</h1>} />
-      </Routes>
+      <div className="h-[87vh] w-full">
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/login" element={<h1>Login</h1>} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+          <Route path="/profile" element={<h1>Profile</h1>} />
+          <Route path="/settings" element={<h1>Settings</h1>} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+          <Route path="/:userId" element={<h1>User Profile</h1>} />
+          <Route path="/:userId/edit" element={<h1>Edit Profile</h1>} />
+        </Routes>
+      </div>
     </div>
   );
 }
