bae4f963ea073fbab5663c0255f1c7b697ec80f7
samirbenabdallah <samirbenabdallah4002@gmail.com>
2025-04-27 09:38:49 +0100

    download packages and update client port

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: "::",
+    port: 3001,
+  },
   plugins: [react(), tailwindcss()],
 });
